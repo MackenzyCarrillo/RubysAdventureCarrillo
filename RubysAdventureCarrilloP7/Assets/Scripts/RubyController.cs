bfdb87e677fe56ba7bf36d56879db46a4702eeaf
MackenzyCarrillo <709666@student.dusd.net>
2022-11-14 15:29:08 -0800

    Added collison to prefabs
    added colision to prefabs . Added rigidbody to ruby and metal boxes

--- RubysAdventureCarrilloP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureCarrilloP7/Assets/Scripts/RubyController.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+
+    Rigidbody2D rigidbody2d;
+    float Horizontal;
+    float Vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float Horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3f * Horizontal * Time.deltaTime;
         position.y = position.y + 3f * Vertical * Time.deltaTime;
-        transform.position = position;
+
+
+        rigidbody2d.MovePosition(position);
     }
+
+ 
+    void ChangeHeanth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+
+    }
+    
+    
+    
+    
+ 
 }
+ 
