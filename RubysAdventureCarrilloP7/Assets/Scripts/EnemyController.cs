1637983c290ea11ba3056af33a8d3b77504e74d6
MackenzyCarrillo <709666@student.dusd.net>
2022-12-06 12:21:33 -0800

    M
    M

--- RubysAdventureCarrilloP7/Assets/Scripts/EnemyController.cs
+++ RubysAdventureCarrilloP7/Assets/Scripts/EnemyController.cs
@@ -81,7 +81,7 @@ public class EnemyController : MonoBehaviour
     public void Fix()
     {
         broken = false;
-        GetComponent<Rigidbody2D>().simulated = false;
+        rigidbody2D.simulated = false;
 
     }
 
--- RubysAdventureCarrilloP7/Assets/Scripts/Projectile.cs
+++ RubysAdventureCarrilloP7/Assets/Scripts/Projectile.cs
@@ -22,7 +22,7 @@ public class Projectile : MonoBehaviour
         EnemyController e = other.collider.GetComponent<EnemyController>();
         if (e != null)
         {
-            e.Fix()
+            e.Fix();
 ;       }
         Destroy(gameObject);
     }
